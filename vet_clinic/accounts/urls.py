ecdbb40fdbfc9d4c37e45134609a988ceaed258d
Kumetsa <i.e.ivanov1989@gmail.com>
2024-03-23 17:38:34 +0200

    Created Login, Logout and User creation

--- vet_clinic/accounts/urls.py
+++ vet_clinic/accounts/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from vet_clinic.accounts.views import LoginUserView, CreateUserView, logout_user
+
+urlpatterns = [
+    path('login/', LoginUserView.as_view(), name='login_user'),
+    path('create_user/', CreateUserView.as_view(), name='create_user'),
+    path('logout/', logout_user, name='logout_user'),
+]
--- vet_clinic/accounts/views.py
+++ vet_clinic/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views, login, logout
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic as views
 
-# Create your views here.
+from vet_clinic.accounts.forms import VetClinicUserCreationForm
+
+
+class LoginUserView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+
+
+class CreateUserView(views.CreateView):
+    form_class = VetClinicUserCreationForm
+    template_name = 'accounts/create_user.html'
+
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        # we call login to log the user
+        login(self.request, form.instance)
+
+        return result
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
